918208596c1f38eead82ec0c7b4a1f4bd9ae8a28
root <root@CV>
2025-03-16 01:18:01 +0530

    added async operation login and client file

--- chatRoom.cpp
+++ chatRoom.cpp
@@ -28,30 +28,53 @@ void Room::deliver(ParticipantPointer participant, Message &message)
     }
 }
 
-void Session::write(Message &message)
+void Session::async_read()
 {
-    messageQueue.push_back(message);
-    while (messageQueue.size() > 0)
+    auto self(shared_from_this());
+    boost::asio::async_read_until(clientSocket, buffer, "\n",
+    [this, self](boost::system::error_code ec, std::size_t bytes_transferred)
     {
-        Message msg = messageQueue.front();
-        messageQueue.pop_front();
-        bool header_decode = message.decodeHeader();
-        if (header_decode)
+        if (!ec)
         {
-            std::string body = message.getBody();
-            async_write(body, message.getBodyLength());
-        }
+            std::string data(boost::asio::buffers_begin(buffer.data()),
+                            boost::asio::buffers_begin(buffer.data()) + bytes_transferred);
+            buffer.consume(bytes_transferred);
+            std::cout << "Received: " << data << std::endl;
 
+            Message message(data);
+            deliver(message);
+            async_read();
+        }
         else
         {
-            std::cout << "Message exceeds the maximum length" << std::endl;
+            room.leave(shared_from_this());
+            if (ec == boost::asio::error::eof)
+            {
+                std::cout << "Connection closed by peer" << std::endl;
+            }
+            else
+            {
+                std::cout << "Read error: " << ec.message() << std::endl;
+            }
         }
-    }
+    });
 }
 
-void Session::deliver(Message &message)
+void Session::async_write(std::string messageBody, size_t messageLength)
 {
-    room.deliver(shared_from_this(), message);
+    auto self(shared_from_this());
+    auto write_handler = [self](boost::system::error_code ec, std::size_t bytes_transferred)
+    {
+        if (!ec)
+        {
+            std::cout << "Data is written to the socket" << std::endl;
+        }
+        else
+        {
+            std::cerr << "Write error: " << ec.message() << std::endl;
+        }
+    };
+    boost::asio::async_write(clientSocket, boost::asio::buffer(messageBody, messageLength), write_handler);
 }
 
 void Session::start()
@@ -60,34 +83,65 @@ void Session::start()
     async_read();
 }
 
+Session::Session(tcp::socket s, Room &r) : clientSocket(std::move(s)), room(r) {};
+
+void Session::write(Message &message)
+{
+    bool header_decode = message.decodeHeader();
+    if (header_decode)
+    {
+        std::string body = message.getBody();
+        async_write(body, message.getBodyLength());
+    }
+    else
+    {
+        std::cout << "Message length exceeds the max length" << std::endl;
+    }
+}
+
+void Session::deliver(Message &incomingMessage)
+{
+    room.deliver(shared_from_this(), incomingMessage);
+}
 using boost::asio::ip::address_v4;
 
 void accept_connection(boost::asio::io_context &io, char *port, tcp::acceptor &acceptor, Room &room, const tcp::endpoint &endpoint)
 {
-    tcp::socket(io);
-    acceptor.async_accept([&](boost::system::error_code ec, tcp::socket socket){
-    if (!ec) {
-        std::shared_ptr<Session> session = std::make_shared<Session>(std::move(socket), room);
-        session->start();
-    }
-        accept_connection(io, port, acceptor, room, endpoint);
-    });
+    acceptor.async_accept(
+        [&io, port, &acceptor, &room, &endpoint](boost::system::error_code ec, tcp::socket socket)
+        {
+            if (!ec)
+            {
+                std::make_shared<Session>(std::move(socket), room)->start();
+            }
+            accept_connection(io, port, acceptor, room, endpoint);
+        });
 }
 
-int main(int argc, char *argv[]){
-
-    try{
-        if(argc < 2){
-            std::cerr<<"Usage: Group <port> <ip>"<<std::endl;
+int main(int argc, char *argv[])
+{
+    try
+    {
+        if (argc < 2)
+        {
+            std::cerr << "Usage: server <port>\n";
             return 1;
         }
 
-        Room room;
         boost::asio::io_context io_context;
-        tcp::endpoint endpoint(address_v4::from_string(argv[2]), std::atoi(argv[1]));
+        Room room;
+        
+        tcp::endpoint endpoint(tcp::v4(), std::atoi(argv[1]));
         tcp::acceptor acceptor(io_context, endpoint);
-        accept_connection(io_context,argv[1],acceptor,room,endppint);
+        
+        std::cout << "Server started on port " << argv[1] << std::endl;
+        
+        accept_connection(io_context, argv[1], acceptor, room, endpoint);
         io_context.run();
-
-    } 
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+    return 0;
 }
--- chatroom.hpp
+++ chatroom.hpp
@@ -12,44 +12,50 @@
 
 using boost::asio::ip::tcp;
 
-class Participant{
+class Participant
+{
 
-    public:
-        virtual void deliver(Message &message) = 0;
-        virtual void write(Message &message) = 0;
-        virtual ~Participant() = default;
+public:
+    virtual void deliver(Message &message) = 0;
+    virtual void write(Message &message) = 0;
+    virtual ~Participant() = default;
 };
 
 typedef std::shared_ptr<Participant> ParticipantPointer;
 
-class Room {
+class Room
+{
 
-        public:
-            virtual join(ParticipantPointer participant);
-            virtual leave(ParticipantPointer participant);
-            virtual deliver(ParticipantPointer participantPointer , Message &message);
+public:
+    virtual void join(ParticipantPointer participant);
+    virtual void leave(ParticipantPointer participant);
+    virtual void deliver(ParticipantPointer participantPointer, Message &message);
 
-        private:
-            std::deque<Message> messageQueue;
-            enum {MaxParticipants = 100};
-            std::set<ParticipantPointer> participants;
+private:
+    std::deque<Message> messageQueue;
+    enum
+    {
+        MaxParticipants = 100
+    };
+    std::set<ParticipantPointer> participants;
 };
 
-class Session: public Participant , public std::enable_shared_from_this<Session> {
+class Session : public Participant, public std::enable_shared_from_this<Session>
+{
 
-    public:
-        Session(tcp::socket s , Room &room);
-        void start();
-        void deliver(Message &message);
-        void write(Message &message);
-        void async_read();
-        void async_write(std::string messageBody, size_t messageLength);
-    
-    private:
-        tcp::socket clientSocket;
-        boost::asio::streambuf buffer;
-        Room &room;
-        std::deque<Message> messageQueue;
+public:
+    Session(tcp::socket s, Room &room);
+    void start();
+    void deliver(Message &message);
+    void write(Message &message);
+    void async_read();
+    void async_write(std::string messageBody, size_t messageLength);
+
+private:
+    tcp::socket clientSocket;
+    boost::asio::streambuf buffer;
+    Room &room;
+    std::deque<Message> messageQueue;
 };
 
 #endif CHATROOM_HPP
